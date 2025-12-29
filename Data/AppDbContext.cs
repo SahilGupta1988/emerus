71834f642b2467b38f98c669ad67e85f339aa5cc
KunalGupta <g.kunal9@gmail.com>
2025-12-30 03:42:40 +0530

    Insert RequestedByUpn & UserId dynamically in ContractorRequest table

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,6 +17,7 @@ namespace Emerus.ETM.Admin.Data
         public DbSet<ContractorHardwareRequest> ContractorHardwareRequest { get; set; } = null!;
         public DbSet<ContractorAccessRequest> ContractorAccessRequest { get; set; } = null!;
         public DbSet<ContractorDocument> ContractorDocument { get; set; } = null!;
+        
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
